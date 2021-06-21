52b43e0acf9586a9eed2679e85bcf4c7e7a51b5b
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-06-21 14:04:21 +0700

    Get list of dependencies in project

--- app/src/main/java/com/rujirakongsomran/kt_opensourcenotices/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_opensourcenotices/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.rujirakongsomran.kt_opensourcenotices
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import com.rujirakongsomran.kt_opensourcenotices.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            startActivity(Intent(this, OssLicensesMenuActivity::class.java))
+        }
     }
 }
