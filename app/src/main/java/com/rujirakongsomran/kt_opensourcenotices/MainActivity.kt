8a114a6d845575b437618972d76d69ae6e6ff6a6
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-06-21 11:19:57 +0700

    Initial commit

---
+++ app/src/main/java/com/rujirakongsomran/kt_opensourcenotices/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rujirakongsomran.kt_opensourcenotices
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
